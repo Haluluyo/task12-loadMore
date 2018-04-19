65b1b44a56cdfed2a43709af8029a3de3cf3d7c9
halulu <lulijuan_00@163.com>
2018-04-19 22:21:34 +0800

    Update router.js

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ app.get('/loadMore',function(req,res){
   var len=req.query.length;
   var data=[]
   for(var i=0;i<len;i++){
-    data.push('新闻'+(parseInt(curIdx)+i))
+    data.push('内容'+(parseInt(curIdx)+i))
   }
   res.send(data)
 })
